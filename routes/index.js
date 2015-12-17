62adf1e3806abd54aac0afdec6bdbe690f3da2b8
Manbir Gulati <mgulati@princeton.edu>
2015-12-16 19:37:08 -0500

    Fix Plot Fix Algorithm Complete working base Fix minor CSS issues

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var randomstring = require("randomstring");
+var Parse = require('node-parse-api').Parse;
+var bodyParser = require('body-parser'), parseURLEncoded = bodyParser.urlencoded({extended: false});
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+var APP_ID = "Ox3Vn0LxOLOJ3AOeFLBrFR4EiNcmPI4IdQPP0pOc";
+var MASTER_KEY = "NDfrYlzoWoRjUcvyYuWbHzYtWFBwgXZ8j6GiO0Zj";
+
+var app = new Parse(APP_ID, MASTER_KEY);
+
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/generate', function(req, res, next) {
+router.get('/generate', function(req, res) {
   var rand = Math.round((Math.random() * 2) + 3);
-  console.log(randomstring.generate({length: rand, charset: req.query.set}));
   res.json({sequence:randomstring.generate({length: rand,charset: req.query.set})});
+});
+
+router.post('/record',function(req, res) {
+  var data = req.body;
+  app.insert('Trial',
+  { age: parseFloat(data.age),
+    score: parseFloat(data.score),
+    time: parseFloat(data.time)},
+  function (err, response) {
+  });
+});
+
+router.get('/plot',function(req, res) {
+app.find('Trial', '', function (error, response) {
+      var arr = response[Object.keys(response)[0]];
+      var data = new Array(arr.length+1);
+      data[0] = ["Time","Score"];
+      for(var i = 0; i<arr.length;i++) {
+        data[i+1] = [Math.round(arr[i].time*100)/100,Math.round(arr[i].score*100)/100];
+      }
+
+      res.json({"data":data});
+  });
 });
 
 module.exports = router;
